8a5113457432867a2cdd1f81d82f8a023b0805ca
Aleexmarcelo <jojoanete@gmail.com>
2021-09-26 21:16:05 -0300

    Adicionar arquivos de projeto.

---
+++ Arrays/Lista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class Lista
+    {
+
+        int[] myIntArray = new int[3];
+
+
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input para inserir os dados para calculos
+            Console.WriteLine("Quantas pessoas serão? : ");
+            float x = int.Parse(Console.ReadLine());
+            double[] Doublearray = new double[x];
+
+            //Loop até chegar no numero desejado
+            for (int i = 0; i < x; i++)
+
+            {
+                //Input para inserir a altura
+                Console.WriteLine("Digite a altura delas : ");
+                double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Doublearray[i] = n;
+            }
+            //Valor final          
+            double a = Doublearray.Sum();
+            Console.WriteLine("A média de altura das pessoas, é  : " + (a / x));
+
+        }
+    }
+}
